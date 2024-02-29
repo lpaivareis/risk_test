2f1a877d9b9999253f93d254d65ad1f24ad7b59b
Lucas Felipe <lucasfelipedepaiva2@gmail.com>
2024-02-29 20:48:20 -0300

    Add `Fraud::ValidationsService`

---
+++ app/services/fraud/validations_service.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module Fraud
+  class ValidationsService < ApplicationService
+    def recent_refund?(user_id)
+      Transaction.where(
+        user_id:,
+        transaction_date: 3.day.ago..Time.current,
+        has_cbk: true
+      ).any?
+    end
+
+    def weekend?(transaction_date)
+      transaction_date.saturday? || transaction_date.sunday?
+    end
+
+    def night?(transaction_date)
+      transaction_date.hour < 6 || transaction_date.hour > 19
+    end
+
+    def high_amount?(transaction_amount)
+      transaction_amount > 500
+    end
+
+    def frequent_user?(user_id)
+      Transaction.where(user_id:, transaction_date: 2.hours.ago..Time.zone.now).count > 5
+    end
+  end
+end
---
+++ spec/services/fraud/validations_service_spec.rb
@@ -0,0 +1,111 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Fraud::ValidationsService, type: :service do
+  describe '#recent_refund?' do
+    before do
+      create_list(:transaction, 3, :with_cbk, user_id: 123_456, transaction_date: 3.hours.ago)
+    end
+
+    let(:transaction) { create(:transaction, user_id: 123_456) }
+
+    subject { described_class.new.recent_refund?(transaction.user_id) }
+
+    it 'returns true if there is a recent refund' do
+      expect(subject).to be_truthy
+    end
+
+    context 'when there is no recent refund' do
+      before do
+        Transaction.where(user_id: 123_456).update_all(has_cbk: false)
+      end
+
+      it 'returns false' do
+        expect(subject).to be_falsey
+      end
+    end
+  end
+
+  describe '#weekend?' do
+    let(:transaction) { create(:transaction, transaction_date: Time.zone.local(2024, 3, 3, 12, 0, 0)) }
+
+    subject { described_class.new.weekend?(transaction.transaction_date) }
+
+    it 'returns true if the transaction date is on a weekend' do
+      expect(subject).to be_truthy
+    end
+
+    context 'when the transaction date is not on a weekend' do
+      let(:transaction) { create(:transaction, transaction_date: Time.zone.local(2024, 3, 5, 12, 0, 0)) }
+
+      it 'returns false if the transaction date is not on a weekend' do
+        expect(subject).to be_falsey
+      end
+    end
+  end
+
+  describe '#night?' do
+    let(:transaction) { create(:transaction, transaction_date: Time.zone.local(2024, 3, 3, 23, 0, 0)) }
+
+    subject { described_class.new.night?(transaction.transaction_date) }
+
+    it 'returns true if the transaction date is during the night' do
+      expect(subject).to be_truthy
+    end
+
+    context 'when the transaction date is not during the night' do
+      let(:transaction) { create(:transaction, transaction_date: Time.zone.local(2024, 3, 3, 12, 0, 0)) }
+
+      it 'returns false if the transaction date is not during the night' do
+        expect(subject).to be_falsey
+      end
+    end
+  end
+
+  describe '#high_amount?' do
+    let(:transaction) { create(:transaction, :high_amount) }
+
+    subject { described_class.new.high_amount?(transaction.transaction_amount) }
+
+    it 'returns true if the transaction amount is high' do
+      expect(subject).to be_truthy
+    end
+
+    context 'when the transaction amount is not high' do
+      let(:transaction) { create(:transaction) }
+
+      it 'returns false if the transaction amount is not high' do
+        expect(subject).to be_falsey
+      end
+    end
+  end
+
+  describe '#frequent_user?' do
+    before do
+      create_list(:transaction, 5, user_id: 123_456, transaction_date: 1.hours.ago)
+    end
+
+    let(:transaction) { create(:transaction, user_id: 123_456, transaction_date: 1.hour.ago) }
+
+    subject { described_class.new.frequent_user?(transaction.user_id) }
+
+    it 'returns true if the user is a frequent user' do
+      expect(subject).to be_truthy
+    end
+
+    context 'when the user is not a frequent user' do
+      before do
+        Transaction.where(user_id: 123_456).delete_all
+      end
+
+      let(:transaction) { create(:transaction, user_id: 123_456) }
+
+      subject { described_class.new.frequent_user?(transaction.user_id) }
+
+      it 'returns false if the user is not a frequent user' do
+        expect(subject).to be_falsey
+      end
+    end
+  end
+end
