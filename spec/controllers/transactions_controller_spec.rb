0f79501e3d089c6e44775528bc847f8dfe4f15c4
Lucas Felipe <lucasfelipedepaiva2@gmail.com>
2024-02-29 22:45:26 -0300

    Add `TransactionControllerSpec`

---
+++ spec/controllers/transactions_controller_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe TransactionsController, type: :controller do
+  describe '#import_transactions' do
+    let(:file) { fixture_file_upload('spec/fixtures/files/transactional-sample.csv', 'text/csv') }
+    let(:limit) { 100 }
+
+    it 'imports transactions successfully' do
+      post :import_transactions, params: { file:, limit: }
+
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to eq({ message: 'Transactions imported successfully' }.to_json)
+    end
+  end
+
+  describe '#check_transaction' do
+    let(:transaction_params) do
+      {
+        transaction_id: '123',
+        merchant_id: '456',
+        user_id: '789',
+        card_number: '123456789',
+        transaction_date: '2022-01-01',
+        transaction_amount: 100.0,
+        device_id: 'abc123'
+      }
+    end
+
+    it 'processes transaction successfully' do
+      allow(ProcessTransactionService).to receive(:call).and_return({ status: 'success' })
+
+      post :check_transaction, params: { transaction: transaction_params }
+
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to eq({ status: 'success' }.to_json)
+    end
+  end
+end
